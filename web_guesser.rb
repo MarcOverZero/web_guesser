1128f6366ca4145bc662782037f13e9dae096894
marcoverzero <marcoverzero@gmail.com>
2017-04-28 08:40:18 -0600

    I4: color coded guessing

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+NUMBER = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  color = background(message)
+  erb :index, :locals => {:number => NUMBER, :message => message, :color => color}
+end
+
+def check_guess(guess)
+  if guess > NUMBER
+    guess > NUMBER + 5 ? "Way too high!" : "Too high!"
+  elsif guess < NUMBER
+    guess < NUMBER - 5 ? "Way too low!" : "Too low!"
+  elsif guess == NUMBER
+    "You got it right! The SECRET NUMBER is #{NUMBER}"
+  end
+end
+
+def background(message)
+  if message == "Way too high!" || message == "Way too low!"
+    "red"
+  elsif message == "Too high!"  || message == "Too low!"
+    "#F75D59" # Bean Red
+  elsif message == "You got it right! The SECRET NUMBER is #{NUMBER}"
+    "green"
+  end
 end
